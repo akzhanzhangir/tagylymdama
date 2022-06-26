26ae12dde6581ee1f760fde0244e553146f65ddb
djakish <69299717+djakish@users.noreply.github.com>
2022-06-26 16:47:39 +0600

    init

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+      <footer className="p-4 h-10  text-black mt-5 ">
+        <Link href="https://github.com/djakish"> my github </Link>
+      </footer>
+    );
+  }
---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Anek+Latin&family=Spline+Sans+Mono:wght@300;400;500&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <meta name="description" content="IT/CS/SE Стажировки в Казахстане" />
+          <meta property="og:title" content="Казахстан IT Стажировки" />
+          <meta property="og:description" content="IT/CS/SE Стажировки в Казахстане" />
+          <meta property="og:url" content="https://tagylymdama.vercel.app/" />
+          <meta property="og:type" content="website" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

85f12f460e4659ffc7ba214ef445aae5a82483aa
akzhanzhangir <69299717+akzhanzhangir@users.noreply.github.com>
2023-09-11 23:08:48 +0600

    font package problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { Martian_Mono } from 'next/font/google'
+import { Martian_Mono } from '@next/font/google'
 
 function cn(...classes: (string | undefined | null | false)[]) {
   return classes.filter(Boolean).join(" ");
